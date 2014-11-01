0a827d19429fc214da5d5a3dcb458a88518a32d0
Christopher M Lyver <lyverc@burninrubber.ccs.neu.edu>
2014-11-01 17:27:15 -0400

    Set up our packet stuct template

--- 3600dns.h
+++ 3600dns.h
@@ -11,3 +11,37 @@
 
 #endif
 
+// We define all the components of a DNS Packet
+
+// The header
+typedef struct header_s{
+	int ID;
+	int QR;
+	int OPCODE;
+	int AA;
+	int TC;
+	int RD;
+	int RA;
+	int Z;
+	unsigned int QDCOUNT;
+	unsigned int ANCOUNT;
+	unsigned int NSCOUNT;
+	unsigned int ARCOUNT;
+}header;
+	
+// The question
+typedef struct question_s{
+	int QNAME;
+	int QTYPES;
+	int QCLASS;
+}question;
+
+// The answer
+typedef struct answer_s{
+	int NAME;
+	int TYPE;
+	int CLASS;
+	int TTL;
+	int RDLENGTH;
+	int RDATA;
+}answer;
